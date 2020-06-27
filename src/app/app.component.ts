9cca044fd43a1be9afb01d2f713f939f02729c9e
Zinedine Menadjelia <zinedinemenadjelia@gmail.com>
2020-06-27 18:45:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
+import { Capability } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meteo';
+  response: string;
+  nouvelleResponse: string;
+  nouveauWeather;
+  weather;
+  insee: string;
+  LAMETEO: number;
+
+  constructor() {
+    const city = 'Paris';
+    this.getInsee(city);
+  }
+  getInsee(city) {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://api.meteo-concept.com/api/location/cities?token=9a237018e181a5975690cfb562ce2d50c0d1726ea51668d42e3f4b9507d028a3&search=' + city);
+    request.responseType = 'text';
+    request.onload = () => {
+      this.response = request.response;
+      this.weather = JSON.parse(this.response);
+      this.insee = this.weather.cities[0].insee;
+      this.getActuel();
+    };
+    request.send();
+  }
+
+  getActuel() {
+    const query = new XMLHttpRequest();
+    query.open('GET', 'https://api.meteo-concept.com/api/forecast/nextHours?token=9a237018e181a5975690cfb562ce2d50c0d1726ea51668d42e3f4b9507d028a3&insee=' + this.insee);
+    query.responseType = 'text';
+    query.onload = () => {
+      this.nouvelleResponse = query.response;
+      this.nouveauWeather = JSON.parse(this.nouvelleResponse);
+      this.LAMETEO = this.nouveauWeather.forecast[0].temp2m;
+    };
+    query.send();
+  }
+
+
+
 }
